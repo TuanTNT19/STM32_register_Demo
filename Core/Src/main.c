2b5c074e415f7b1fdf3d7e53ef43ef144d024750
TuanTNT19 <tuantnt08@gmail.com>
2023-09-13 23:33:10 +0700

    update in USART

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,60 +1,30 @@
-void TIM2_conf()
+#include "stm32f10x.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+uint8_t buffer[30];
+
+uint8_t datareceive[8];
+void TIM2_config()
 {
-<<<<<<< HEAD
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-	TIM2->PSC=7999;
-	TIM2->CR1 |=(1<<0);
-=======
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; //Enable TIMEeR2 clock
 	TIM2->PSC = 7;
 	TIM2->ARR = 65535;
 	TIM2->CR1 = (1<<0);//enable counter
->>>>>>> USART
 	while (!(TIM2->SR & TIM_SR_UIF));
+}
+void delay_us(uint16_t us)
+{
+	TIM2->CNT=0;
+	while (TIM2->CNT < us);
 }
 void delay_ms(uint16_t ms)
 {
-	TIM2->CNT=0;
-	while(TIM2->CNT <= ms);
-}
-void TIM3_PWM_Init()
-{
-	// config GPIOA_PIN7 as a output
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-  GPIOA->CRL &= 0x0FFFFFFF;
-	GPIOA->CRL |= (10<<28);
-	// set pwm
-	// default duty set by CCR2 =0
-	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; 
-	TIM3->PSC= 7;
-	TIM3->ARR = 999;
-	TIM3->CCER |= (1<<4);
-	TIM3->CCMR1 |= (6<<12);
-	TIM3->CCR2=0;
-	TIM3->CR1 |=(1<<0);
-	while (!(TIM3->SR & TIM_SR_UIF));
-}
-void setduty(uint16_t duty)
-{
-	TIM3->CCR2 = duty;
-}
-<<<<<<< HEAD
-int main(void)
-{
-	
-	TIM2_conf();
-	TIM3_PWM_Init();
-	while(1)
+	while (ms--)
 	{
-		for (int i=0;i<1000;i+=5)
-		{
-			setduty(i);
-			delay_ms(10);
-		}
-		
+		delay_us(1000);
 	}
-	
-=======
+}
 void GPIO_config()
 {
 	RCC->APB2ENR |= (1<<2);//enable clock for gpioa
@@ -63,8 +33,11 @@ void GPIO_config()
 	GPIOA-> CRL |= (10<<8);
 		// config gpioa pin 3 as alternative function RX
 	GPIOA->CRL &= (0xFFFF0FFF);
-	GPIOA->CRL |= (4<<12);
-	
+	GPIOA->CRL |= (8<<12);//in push pull up/down
+	GPIOA->ODR |= (1<<3);// pull up
+	RCC->APB2ENR |= (1<<4);
+	GPIOC-> CRH &= (0xFF0FFFFF);
+	GPIOC-> CRH |= (2<<20);
 
 }
 void USART2_config()
@@ -72,7 +45,8 @@ void USART2_config()
 	RCC->APB1ENR |= (1<<17);//enable clock for USART2
 	USART2->BRR |= 833;
 	USART2->CR1 |= (1<<13);
-	USART2->CR1 |=(1<<3);
+	USART2->CR1 |= (1<<2); // RE=1.. Enable the Receiver
+  USART2->CR1 |= (1<<3);  // TE=1.. Enable Transmitter
 	
 }
 void USART2_Send_char(char str)
@@ -87,24 +61,53 @@ void USART2_Send_String(char *ch)
 		 USART2_Send_char(*ch++);
 	}
 }
-//void USART2_Receive_data(char *k)
-//{
-//		while (!(USART2->SR & USART_SR_RXNE));
-//	  *k = USART2->DR;
-//	  *k++;
-//}
+
+uint8_t USART_Getchar(void)
+{
+	
+	uint8_t Temp;
+	while (!(USART2->SR & USART_SR_RXNE));
+	Temp = USART2->DR;
+	return Temp;
+}
+void UART_Getstring(uint8_t *str, uint8_t len)
+{
+	for (uint8_t i=0;i<len;i++)
+	{
+		*str=USART_Getchar();
+				if (*str=='n')
+			{
+				*(str+1)=NULL;
+				break;
+				}
+		str++;
+
+
+	}
+	
+	
+}
 int main(void)
 {
 	GPIO_config();
 	USART2_config();
 	TIM2_config();
+	GPIOC->ODR |= (1<<13);
 	while(1)
 	{
-//	  char Rstring[30];
-//		USART_Receive_data(&Rstring);
-		USART2_Send_String("Stringabcd ");
-		delay_ms(1000);
+
+	UART_Getstring(datareceive,7);
+		USART2_Send_String(datareceive);
+		if (strcmp(datareceive, "led_on")==0 )
+			{
+				GPIOC->ODR &=~ (1<<13);
+				 
+				}
+			else 
+				{
+					GPIOC->ODR |= (1<<13);
+				}
 		
-	}
->>>>>>> USART
+		
+}
 }
